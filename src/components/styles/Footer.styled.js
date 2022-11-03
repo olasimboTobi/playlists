914b7b22b42512ad088b4522c96a8982272738db
Tobi Olasimbo <tobiolasimbo84@gmail.com>
2022-11-03 17:52:30 +0100

    updated a component

--- src/components/styles/Footer.styled.js
+++ src/components/styles/Footer.styled.js
@@ -8,7 +8,7 @@ export const StyledFooter = styled.div`
    @media screen and (min-width: 280px) and (max-width: 780px){
     width:100%;
     background-color:rgb(0,0,0);
-    margin:1em 0em;
+    margin:2em 0em;
     font-size:2em;
     color:white;
     display:flex;
