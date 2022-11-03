dcf8bbf5d2b0bc2a38bfc376b1825c64f9ef8250
Tobi Olasimbo <tobiolasimbo84@gmail.com>
2022-11-03 16:40:23 +0100

    added gsap to framephoto component

--- src/components/FramePhoto.js
+++ src/components/FramePhoto.js
@@ -1,12 +1,26 @@
-import React,{useState} from 'react'
+import React,{useEffect, useRef} from 'react'
+import {TweenMax, Power3} from "gsap"
 import { StyledFramePhoto  } from './styles/FramePhoto.styled'
 
 
 function FramePhoto({ title,colors, text, imgUrl}) {
+  let frameItem = useRef(null)
 
+  useEffect(()=> {
+    TweenMax.to(
+      frameItem,
+      0.8,
+      {
+        opacity:1,
+        y:20,
+        ease: Power3.easeOut,
+        delay:0.2
+      }
+    )
+  })
 
   return (
-    <StyledFramePhoto>
+    <StyledFramePhoto ref={el => {frameItem = el}}>
         <div className="imgAlbum"><img src={imgUrl}/></div>      
         <div className="black--box" >""</div>
         <div className="linear--box" style={{ background:`linear-gradient(${colors})`}}>
