05177e1376a9bd56a91fa614709e5f5b97825b28
Tobi Olasimbo <tobiolasimbo84@gmail.com>
2022-11-04 05:58:59 +0100

    added scroll effect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
+import React, { useEffect } from "react";
 import { Container } from './components/styles/Container.styled'
 import Sidebar from './components/Sidebar'
 import Body from './components/Body'
 import Footer from './components/Footer'
+import scrollreveal from "scrollreveal";
 
 
 function App() {
+
+  useEffect(() => {
+    const sr = scrollreveal({
+      origin: "bottom",
+      distance: "80px",
+      duration: 2000,
+      reset: false,
+    });
+    sr.reveal(
+      `
+        #sidebar,
+        #body,
+        #footer
+    `,
+      {
+        opacity: 0,
+        interval: 500,
+      }
+    );
+  }, []);
+
+  
   return (
     <Container className="player__body">
       <Sidebar/>
